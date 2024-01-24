d7d1a75b87580fd317307b38dca34b19be2e9427
Slaveik-1 <125739630+Slaveik-1@users.noreply.github.com>
2024-01-25 00:14:04 +0400

    Add files via upload

--- src/test/java/framework/managers/WebDriverManager.java
+++ src/test/java/framework/managers/WebDriverManager.java
@@ -13,62 +13,54 @@ import java.util.concurrent.TimeUnit;
 public class WebDriverManager {
     public static final String dType = "remote";
     public static final String selen = "http://149.154.71.152:4444/wd/hub";
+    public static final String Btype = "chrome";
 
     private static WebDriver webDriver = null;
     public static RemoteWebDriver rDriver;
     private WebDriverManager(){
-
     }
 
     public static void initDriver() {
         if (dType.equals("local")) {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
             webDriver = new ChromeDriver();
-       /* System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        webDriver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);*/
         } else if (dType.equals("remote")) {
             DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-            desiredCapabilities.setBrowserName(dType);
+            desiredCapabilities.setBrowserName(Btype);
+            desiredCapabilities.setVersion("109.0");
             desiredCapabilities.setCapability("selenoid:options", Map.<String, Object>of(
                     "enableVNC", true,
                     "enableVideo", false
             ));
             try {
-                rDriver = new RemoteWebDriver(
-                        URI.create(selen).toURL(),
-                        desiredCapabilities);
+                rDriver = new RemoteWebDriver(URI.create(selen).toURL(), desiredCapabilities);
             } catch (MalformedURLException exception) {
                 exception.printStackTrace();
             }
+        }}
+
+
+        public static WebDriver getWebDriver () {
+            if (webDriver == null && rDriver == null) {
+                initDriver();
+            }
+            if (dType.equals("local"))
+                return webDriver;
+            if (dType.equals("remote"))
+                return rDriver;
+            return null;
         }
-    }
 
-
-    public static WebDriver getWebDriver(){
-        if (webDriver == null && rDriver == null){
-            initDriver();
+        public static void quit () {
+            if (webDriver != null) {
+                webDriver.quit();
+                webDriver = null;
+            }
+            if (rDriver != null) {
+                rDriver.quit();
+            }
         }
-        if (dType.equals("local"))
-            return webDriver;
-        if (dType.equals("remote"))
-            return rDriver;
-        return null;
-    }
 
-    public static void quit() {
-        if (webDriver != null){
-            webDriver.quit();
-        webDriver = null;
-    }
-        if (rDriver != null){
-            rDriver.quit();
-        }
     }
 
 
-
-
-}
